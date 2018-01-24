b705b1006f786f8a7cbb468a558ce0925536c922
Albert Vultr <albert.abril@gmail.com>
2018-01-24 22:07:45 +0000

    ordenant i afegint criptmonedes queries

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Telegraf = require('telegraf')
+const axios = require('axios')
+const fs = require('fs')
 const { reply } = Telegraf
 
 const oldSchool = require('./controllers/oldschool')
@@ -10,6 +12,7 @@ const DEBUG = true
 const token = '510500699:AAEAf_fGn0u9mAocdMeSUP5YQLb1fatnz4U' // process.env.BOT_TOKEN
 
 const bot = new Telegraf(token)
+
 bot.command('/oldschool', (ctx) => ctx.reply('Hello'))
 bot.command('/modern', ({ reply }) => reply('Yo'))
 bot.command('/hipster', reply('λ'))
@@ -19,13 +22,55 @@ bot.command('/queasco', (ctx) => {
     ctx.telegram.sendAudio(chatId, 'http://st09.ioo.cat/queascoche.mp3')
 }) 
 
+bot.command('/dinar', (ctx) => {
+    const chatId = "-267784992" 
+    ctx.telegram.sendPhoto(chatId, 'http://st09.ioo.cat/dinar.jpg')
+})
+
+bot.command('/dinar', (ctx) => ctx.replyWithPhoto({ source: fs.createReadStream('/home/nvm/apps/parcerisbot/dinar.jpg') }))
+
+bot.command('/bitcoin', (ctx) => {
+   displayBitcoinPrice(ctx)
+})
+
+function displayPriceBitcoin(ctx) {
+   const url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD,EUR"
+   axios.get(url).then((response) => {
+       ctx.reply('El Bitcoin val ara més o menys ' + response.data.EUR  + ' euros / ' + response.data.USD + ' dolars')
+   })  
+}
+
+function displayPriceEthereum(ctx) {
+   const url = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR"
+   axios.get(url).then((response) => {
+       ctx.reply('El ETHEREUM val ara més o menys ' + response.data.EUR  + ' euros / ' + response.data.USD + ' dolars')
+   })  
+}
+
+function displayPriceNeo(ctx) {
+   const url = "https://min-api.cryptocompare.com/data/price?fsym=NEO&tsyms=BTC,USD,EUR"
+   axios.get(url).then((response) => {
+       ctx.reply('El NEO val ara més o menys ' + response.data.EUR  + ' euros / ' + response.data.USD + ' dolars')
+   })  
+}
+
+
+
+
+
 
 if(DEBUG) {
   bot.on('message', (ctx) => {
     //console.log(ctx)
-    console.log(ctx.update.message.chat)
-    console.log(ctx.update.message.text)
-    //const chatId = "-267784992"
+    const chatId = ctx.update.message.chat.id
+    const text = ctx.update.message.text 
+
+    console.log(chatId)
+    console.log(text)
+    
+    if(text.match(/bitcoin/i)) displayPriceBitcoin(ctx)
+    if(text.match(/ethereum/i)) displayPriceEthereum(ctx)
+    if(text.match(/neo/i)) displayPriceNeo(ctx)
   })
 }
 

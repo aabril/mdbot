6e302e39e181408b4b33848665deee4406d1b2b9
Albert Vultr <albert.abril@gmail.com>
2018-01-25 01:08:23 +0000

    tot en marxa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Telegraf = require('telegraf')
 const axios = require('axios')
 const fs = require('fs')
-const { reply } = Telegraf
+const { reply, replyWithAudio } = Telegraf
 
 const oldSchool = require('./controllers/oldschool')
 const modern = require('./controllers/modern')
@@ -15,66 +15,54 @@ const bot = new Telegraf(token)
 
 
 function displayAudioQueAsco(ctx) {
-    
-}
-
-
-
-
-function displayAudioQueAsco(ctx, chatId) {
-    console.log(ctx.update.message.chat.id)
-    ctx.telegram
-	.sendVoice({
-	    chat_id: ctx.update.message.chat.id,
-	    voice: fs.createReadStream('/home/nvm/apps/parcerisbot/queascoche.mp3')
-	})
-	.catch((err) => {
-            console.log(err)
-	})
+    ctx
+      .replyWithAudio('http://st09.ioo.cat/queascoche.mp3')
+      .catch((err) => {
+         console.log(err)
+      })
 }
 
 function displayPhotoDinar(ctx) {
-    const photoPath = '/home/nvm/apps/parcerisbot/dinar.jpg'
-    ctx.replyWithPhoto(ctx.update.message.chat.id, { source: fs.createReadStream(photoPath) })
+    const photoPath = 'dinar.jpg'
+    ctx.replyWithPhoto({ source: photoPath })
 }
 
 function displayPriceBitcoin(ctx) {
-   const url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD,EUR"
+   const url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=GBP,USD,EUR"
    axios.get(url).then((response) => {
-       ctx.reply('El Bitcoin val ara més o menys ' + response.data.EUR  + ' euros / ' + response.data.USD + ' dolars')
+       ctx.reply('El Bitcoin val ara més o menys ' + response.data.EUR  + ' euros / ' + response.data.USD + ' dolars / ' + response.data.GBP+ ' lliures')
    })  
 }
 
 function displayPriceEthereum(ctx) {
-   const url = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR"
+   const url = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=GBP,USD,EUR"
    axios.get(url).then((response) => {
-       ctx.reply('El ETHEREUM val ara més o menys ' + response.data.EUR  + ' euros / ' + response.data.USD + ' dolars')
+       ctx.reply('El Ethereum val ara més o menys ' + response.data.EUR  + ' euros / ' + response.data.USD + ' dolars / ' + response.data.GBP+ ' lliures')
    })  
 }
 
 function displayPriceNeo(ctx) {
-   const url = "https://min-api.cryptocompare.com/data/price?fsym=NEO&tsyms=BTC,USD,EUR"
+   const url = "https://min-api.cryptocompare.com/data/price?fsym=NEO&tsyms=GBP,USD,EUR"
    axios.get(url).then((response) => {
-       ctx.reply('El NEO val ara més o menys ' + response.data.EUR  + ' euros / ' + response.data.USD + ' dolars')
+       ctx.reply('El Neo val ara més o menys ' + response.data.EUR  + ' euros / ' + response.data.USD + ' dolars / ' + response.data.GBP+ ' lliures')
    })  
 }
 
-if(DEBUG) {
-  bot.on('message', (ctx) => {
-    //console.log(ctx)
-    const chatId = ctx.update.message.chat.id
-    const text = ctx.update.message.text 
+bot.on('message', (ctx) => {
 
+  const text = ctx.update.message.text 
+  const chatId = ctx.update.message.chat.id
+
+  if(DEBUG) {
     console.log(chatId)
     console.log(text)
-    
-    if(text.match(/bitcoin/i)) displayPriceBitcoin(ctx)
-    if(text.match(/ethereum/i)) displayPriceEthereum(ctx)
-    if(text.match(/neo/i)) displayPriceNeo(ctx)
-    if(text.match(/dinar/i)) displayPhotoDinar(ctx)
-    if(text.match(/asco/i)) displayAudioQueAsco(ctx)
-  })
-}
+  }
+
+  if(text.match(/bitcoin/i)) displayPriceBitcoin(ctx)
+  if(text.match(/ethereum/i)) displayPriceEthereum(ctx)
+  if(text.match(/neo/i)) displayPriceNeo(ctx)
+  if(text.match(/dinar/i)) displayPhotoDinar(ctx)
+  if(text.match(/asco/i)) displayAudioQueAsco(ctx)
+})
 
 bot.startPolling()
-
